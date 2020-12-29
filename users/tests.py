297d52668c50c146cc4cdbb612aff2a8ebdb8282
fea2fwa <fea2fwa@gmail.com>
2020-12-29 11:13:25 +0900

    ch5

--- users/tests.py
+++ users/tests.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
 class CustomUserTests(TestCase):
 
@@ -31,3 +32,15 @@ class CustomUserTests(TestCase):
         self.assertTrue(admin_user.is_superuser)
 
 
+class SignupPageTests(TestCase):
+
+    def setUp(self):
+        url = reverse('signup')
+        self.response = self.client.get(url)
+
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'signup.html')
+        self.assertContains(self.response, 'Sign Up')
+        self.assertNotContains(
+                self.response, 'Hi there! I shoud not be on the page.')
